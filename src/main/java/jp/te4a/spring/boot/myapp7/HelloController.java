50120cb994105e1e50b12e4235f705b1d6572c40
150293 <150293@e0601-08.JC-21.local>
2018-05-22 14:31:20 +0900

    Commit

--- src/main/java/jp/te4a/spring/boot/myapp7/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp7/HelloController.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp6;
+package jp.te4a.spring.boot.myapp7;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,17 +13,17 @@ public class HelloController {
 	@Autowired
 	BookService bookService;
 	
-	@RequestMapping("/")
+	@RequestMapping("books/list")
 	public String index(Model model) {
 		model.addAttribute("msg", "this is setting message");
-		return "index";
+		return "books/list";
 	}
-	@RequestMapping(value="/post", method=RequestMethod.POST)public ModelAndView postForm(@RequestParam("id") String id,
+	@RequestMapping(value="books/list", method=RequestMethod.POST)public ModelAndView postForm(@RequestParam("id") String id,
 	@RequestParam("title") String title,
 	@RequestParam("writer") String writer,
 	@RequestParam("publisher") String publisher,
 	@RequestParam("price") String price) {
-		ModelAndView mv = new ModelAndView("index");
+		ModelAndView mv = new ModelAndView("books/list");
 		bookService.save(new BookBean(Integer.valueOf(id), title, writer, publisher,
 				Integer.valueOf(price)));
 		StringBuffer buff = new StringBuffer();
@@ -33,7 +33,7 @@ public class HelloController {
            "<BR>"+ "著者:" + bean.getWriter() + "<BR>" + "出版社:" + bean.getPublisher() + 
            "<BR>"+ "価格:" + bean.getPrice() + "<BR><HR>");
         }
-        mv.addObject("msg", buff.toString());
+        mv.addObject("books", bookService.findAll());
         return mv;
 
 	}
