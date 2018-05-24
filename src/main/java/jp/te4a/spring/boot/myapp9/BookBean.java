1255875459a454c904f9198661c288248d82dfba
150293 <150293@e0601-08.JC-21.local>
2018-05-24 11:34:35 +0900

    Commit

---
+++ src/main/java/jp/te4a/spring/boot/myapp9/BookBean.java
@@ -0,0 +1,24 @@
+package jp.te4a.spring.boot.myapp9;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "books")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookBean {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	@Column(nullable = false)
+	private String title;
+	private String writer;
+	private String publisher;
+	private Integer price;
+
+}
